84644f89257217a002bdc37b9eb756717a5d581d
eduardo845 <71361256+eduardo845@users.noreply.github.com>
2021-12-02 10:06:03 -0600

    modificacion1

--- resources/views/auth/menu.blade.php
+++ resources/views/auth/menu.blade.php
@@ -8,15 +8,17 @@
             <span class="usg">UNIDAD DE SERVICIOS GENERALES</span>
         </div>
         <div class="right">
-            <button type="button" class="kardex">KARDEX</button>
-            <button type="button" class="registro">Registro de Datos</button>
-            <button type="button" class="salida">Salidas de Productos</button>
-            <button type="button" class="reporte">Reporte de Mes</button>
-            <button type="button" class="ajuste">Ajustar Saldo</button>
-            <button type="button" class="sal-usg">Salidas USG</button>
-            <button type="button" class="inventario">Inventario</button>
-           
-            
+            <div class="right1">
+                <button type="button" class="kardex">KARDEX</button>
+                <button type="button" class="registro">Registro de Datos</button>
+                <button type="button" class="salida">Salidas de Productos</button>
+                <button type="button" class="reporte">Reporte de Mes</button>
+            </div>
+            <div class="right2">
+                <button type="button" class="ajuste">Ajustar Saldo</button>
+                <button type="button" class="sal-usg">Salidas USG</button>
+                <button type="button" class="inventario">Inventario</button>
+            </div>
         </div>
     </div>
         
